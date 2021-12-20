7aa165346da8ae06533d53b0f19dd4523018839f
Ieonard04 <leonardogilrojo@gmail.com>
2021-12-19 21:28:34 -0800

    Added the trans flag; separated the flag generators from the math-related functions

---
+++ color_math.py
@@ -0,0 +1,45 @@
+
+
+# Linear interpolation
+def lerp(x: float, x0: float, x1: float) -> float:
+    return x0 + x * (x1 - x0)
+
+
+# Cubic interpolation
+def cuberp(x: float, x0: float, x1: float) -> float:
+    x = min(1.0, x)
+    x = max(0.0, x)
+    return x0 + (x1 - x0) * (3 * x ** 2 - 2 * x ** 3)
+
+
+# Interpolation of two colors
+# Formula parameter specifies the interpolation method as a lambda expression
+def interp_color(x: float, color1: tuple, color2: tuple, formula) -> tuple:
+    return formula(x, color1[0], color2[0]), formula(x, color1[1], color2[1]), formula(x, color1[2], color2[2])
+
+
+# casts a color to int values
+def to_int(color: tuple) -> tuple:
+    return int(color[0]), int(color[1]), int(color[2])
+
+
+# Returns a tuple r, g, b calculated from the hsv values given
+def hsv(h: float, s: float, v: float) -> tuple:
+    h %= 360
+    c = v * s
+    x = c * (1 - abs((h / 60) % 2 - 1))
+    m = v - c
+
+    e = int(m * 255)
+    f = int((c + m) * 255)
+    g = int((x + m) * 255)
+    mode = h / 60
+
+    if mode < 1: return f, g, e
+    if mode < 2: return g, f, e
+    if mode < 3: return e, f, g
+    if mode < 4: return e, g, f
+    if mode < 5: return g, e, f
+    if mode < 6: return f, e, g
+
+    return 0, 0, 0  # Will paint pixels with an erroneous mode as black
--- generator.py
+++ generator.py
@@ -1,38 +1,6 @@
-from PIL import Image, ImageFilter
+from PIL import Image
 from numpy import abs, exp
-
-
-def lerp(x: float, x0: float, x1: float) -> float:
-    return x0 + x * (x1 - x0)
-
-
-def lerp_color(x: float, color1: tuple, color2: tuple) -> tuple:
-    return lerp(x, color1[0], color2[0]), lerp(x, color1[1], color2[1]), lerp(x, color1[2], color2[2])
-
-
-def to_int(color: tuple) -> tuple:
-    return int(color[0]), int(color[1]), int(color[2])
-
-# Returns a tuple r, g, b calculated from the hsv values given
-def hsv(h: float, s: float, v: float) -> tuple:
-    h %= 360
-    c = v * s
-    x = c * (1 - abs((h / 60) % 2 - 1))
-    m = v - c
-
-    e = int(m * 255)
-    f = int((c + m) * 255)
-    g = int((x + m) * 255)
-    mode = h / 60
-
-    if mode < 1: return f, g, e
-    if mode < 2: return g, f, e
-    if mode < 3: return e, f, g
-    if mode < 4: return e, g, f
-    if mode < 5: return g, e, f
-    if mode < 6: return f, e, g
-
-    return 0, 0, 0  # Will paint pixels with an erroneous mode as black
+from color_math import to_int, interp_color, cuberp, hsv
 
 
 # Generates an image based on a function defining the color of each pixel
@@ -58,6 +26,7 @@ def bi(image: Image):
     center = image.size[0] / 2 + image.size[1] / 2
 
     # Uses logistical curves to create a smooth transition from magenta to purple to blue
+    # Takes a while to calculate, unfortunately
     curve_magenta = lambda x: -40 / (1 + exp(-0.02 * (x - center + 200)))
     curve_blue = lambda x: -40 / (1 + exp(-0.04 * (x - center - 140)))
     curve_total = lambda x: curve_magenta(x) + curve_blue(x)
@@ -65,18 +34,21 @@ def bi(image: Image):
     return generate(image, lambda x, y: hsv(320 + curve_total(x + y / 2), 0.85, 0.88 - x / 7680))
 
 
+# Trans pride flag gradient
 def trans(image: Image):
+    # Colors used for the flag
     white = 255, 255, 255
     pink = 247, 168, 184
     blue = 85, 205, 252
 
+    # Specifies how to interpolate those colors
     def get_color(x: int, center: int, width: int):
         delta = abs(x - center)
-        if delta < width / 2: return to_int(lerp_color(min(1, max(0, delta - width / 2)), white, pink))
-        if delta < 3/2 * width: return to_int(lerp_color(min(1, max(0, delta - 3/2 * width)), pink, blue))
+        if delta < width / 2:
+            return to_int(interp_color((width / 2 - delta) / 60, pink, white, lambda x, x0, x1: cuberp(x, x0, x1)))
+        if delta < 3/2 * width:
+            return to_int(interp_color((3 * width / 2 - delta) / 60, blue, pink, lambda x, x0, x1: cuberp(x, x0, x1)))
         return blue
 
     center = image.size[0] / 2 + image.size[1] / 2
-    generate(image, lambda x, y: get_color(x + y / 2, center, 500))
-    return image.filter(ImageFilter.GaussianBlur(12))
-
+    return generate(image, lambda x, y: get_color(x + y / 2, center, 500))
