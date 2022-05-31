1235e4b725648a63008dd0da12db451596edd859
Ieonard04 <leonerd04.2718@gmail.com>
2022-05-31 13:28:42 -0700

    Streamlined the addition of new flags using lists, added a benchmarking function

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+from typing import Callable
+
 from generator import *
 import os
+import time
+
+
+# Times the amount of time it takes for a certain function to run
+# returns in milliseconds (ms)
+def benchmark(function: Callable) -> float:
+    start = time.time_ns()
+    function()
+    end = time.time_ns()
+
+    return (end - start) / 1000000.0
 
 
 # The main script just generates all the flags and saves them to a folder
 if __name__ == '__main__':
-    width, height = 1920, 1080
+    width, height = 192, 108
+
+    flags = [
+        lambda: gen_pride_flag(width, height),
+        lambda: gen_gay_flag(width, height),
+        lambda: gen_lesbian_flag(width, height),
+        lambda: gen_bi_flag(width, height),
+    ]
+    names = [
+        "LGBTQ+",
+        "gay (mlm)",
+        "lesbian (wlw)",
+        "bi"
+    ]
+    filenames = [
+        "lgbtq_pride",
+        "gay_pride",
+        "lesbian_pride",
+        "bi_pride"
+    ]
 
     print("Creating out directory...")
 
@@ -12,19 +44,27 @@ if __name__ == '__main__':
         os.mkdir("out")  # os.mkdir("out") tries to make the directory "out" wherever this script was run
         print("Directory created.")
     except FileExistsError:
-        print("Directory already exists. Continuing...\n")
+        print("Directory already exists.")
+        time.sleep(0.3)
+        print("Continuing...\n")
+        time.sleep(0.5)
         pass
 
-    print("Generating LGBTQ+ pride flag...")
-    gen_pride_flag(width, height).save("out/lgbtq_pride.png", "PNG")
+    total_time = 0
 
-    print("Generating gay (mlm) pride flag...")
-    gen_gay_flag(width, height).save("out/gay_pride.png", "PNG")
+    for name, filename, generator in zip(names, filenames, flags):
+        print(f"Generating {name} pride flag...")
+        time_elapsed = benchmark(lambda: generator().save(f"out/{filename}.png", "PNG"))
+        total_time += time_elapsed
 
-    print("Generating lesbian (wlw) pride flag...")
-    gen_lesbian_flag(width, height).save("out/lesbian_pride.png", "PNG")
+        if time_elapsed > 1000:
+            time_elapsed /= 1000.0
+            print(f"Took {time_elapsed:.2f} s.\n")
+        else:
+            print(f"Took {time_elapsed:.2f} ms.\n")
+        time.sleep(1)
 
-    print("Generating bi pride flag...")
-    gen_bi_flag(width, height).save("out/bi_pride.png", "PNG")
+    total_time /= 1000.0
 
-    print("Generation complete. The images can be found in <project directory>/out/")
+    print(f"Generation took {total_time:.2f} s.")
+    print("The generated images can be found in <project directory>/out/")
